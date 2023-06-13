a7e0ffb3bc97c04758e657a769a8da15bf8c9aeb
Yash Vachhani <yashvachhani88@gmail.com>
2023-06-13 18:19:46 +0100

    added requirements and setup files

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8" ) as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "text-summarizer"
+AUTHOR_USER_NAME = "yashvachhani"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "yashvachhani88@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Text Summarizer package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    }
+    package_dir={}
+    packages=setuptools.find_packages(),
