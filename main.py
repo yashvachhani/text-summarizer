58e2c3a68dfff64483ac2209be766f504ce907ca
Yash Vachhani <yashvachhani88@gmail.com>
2023-06-21 17:57:01 +0100

    worked on logs

---
+++ main.py
@@ -0,0 +1,7 @@
+from textSummarizer.logging import logger
+
+print ("we are in main")
+def test_logger():
+    logger.info("test logger")
+
+test_logger()
--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ setuptools.setup(
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    }
-    package_dir={}
+    },
+    package_dir={},
     packages=setuptools.find_packages(),
+
+)
