7dec2c1e4dc094e49e27b1af75e9091dc4e657f4
Yash Vachhani <yashvachhani88@gmail.com>
2023-07-13 17:45:48 +0100

    worked on data transformation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -18,6 +19,17 @@ try:
     logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
     data_validation = DataValidationTrainingPipline()
     data_validation.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_Transformation = DataTransformationTrainingPipeline()
+    data_Transformation.main()
     logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/conponents/data_validation.py
+++ src/textSummarizer/conponents/data_validation.py
@@ -15,12 +15,17 @@ class DataValiadtion:
             for file in all_files:
                 if file not in self.config.ALL_REQUIRED_FILES:
                     validation_status = False
-                    with open(self.config.STATUS_FILE,'w') as f:
+                    print(validation_status)
+                    with open(self.config.STATUS_FILE, 'w') as f:
+                        print(validation_status)
                         f.write(f"Validation status: {validation_status}")
                 else:
                     validation_status = True
-                    with open(self.config.STATUS_FILE,'w') as f:
+                    print(validation_status)
+                    with open(self.config.STATUS_FILE, 'w') as f:
+                        print(validation_status)
                         f.write(f"Validation status: {validation_status}")
+
             return validation_status
         except Exception as e:
             raise e
